8a0df6672d82173cf02737ef2d1175f7ce95e0a4
Marcos Salles <marcosrs805@gmail.com>
2022-06-15 10:05:32 -0300

    atualizando tela e lendo banco de dados

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 'use strict';
 
+let banco = [
+    {"tarefa": "Comprar pão", "status": ""},
+    {"tarefa": "Comprar leite", "status": "checked"}
+]
 
-const criarItem = (texto, status='') => {
+const criarItem = (tarefa, status) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML = `
     <input type="checkbox" ${status}>
-    <div>${texto}</div>
+    <div>${tarefa}</div>
     <input type="button" value="X">
     `
     document.getElementById('todoList').appendChild(item);
 }
+
+const limparTarefas = () => {
+    const lista = document.getElementById('todoList');
+    while (lista.firstChild) {
+        lista.removeChild(lista.lastChild);
+    }
+}
+
+const atualizarTela = () => {
+    limparTarefas();
+    banco.forEach(item => criarItem(item.tarefa, item.status));
+
+}
+
+
+
